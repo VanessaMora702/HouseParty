5a129d4dc9af9ca532cde25351ce0c9b56ff392f
vanessaMora702 <vanessamora702@gmail.com>
2021-03-18 17:40:38 -0400

    installed swaggerUI and set it up accordingly

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('frontend.urls'))
+    path('', include('frontend.urls')),
+    path('openapi/', get_schema_view(
+        title="Music Controller"
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+         template_name='documentation.html',
+        extra_context={'schema_url':'openapi-schema'} 
+    ), name='swagger-ui')
 ]
